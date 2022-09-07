73c6dad87892457ad9b57fbf4851e3269190ebc3
Brandon <BJack0@outlook.com>
2022-09-07 13:33:26 +0100

    Add a working function to create the computers hand

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 console.log("Hello World!")
 
+// Function returns an integer between 0 - 2
+// Function Converts integer into a playable hand
+
 function getComputerChoice() {
   let computerHand = Math.floor(Math.random() * 3)
   console.log(computerHand)
+
+  if (computerHand === 0) {
+    let computerHand = "Rock"
+    console.log(computerHand)
+  } else if (computerHand === 1) {
+    let computerHand = "Paper"
+    console.log(computerHand)
+  } else if (computerHand === 2) {
+    let computerHand = "Scissors"
+    console.log(computerHand)
+  }
 }
