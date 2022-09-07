a41ddafedeef95e9bb42df2b51a37ed74a5caa3d
Brandon <BJack0@outlook.com>
2022-09-07 13:11:44 +0100

    Make Math.random return whole integer

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 console.log("Hello World!")
 
 function getComputerChoice() {
-  let computerHand = Math.random() * 3
+  let computerHand = Math.floor(Math.random() * 3)
   console.log(computerHand)
 }
